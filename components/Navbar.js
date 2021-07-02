e77f0df14591036297abc350ad4faf3387d2c313
oliverloops <oliveralr@protonmail.com>
2021-07-02 11:40:41 -0500

    New layout updates

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="col col-start-1 pl-10">
+      <p>hi</p>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
+//UI components
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-3 justify-items-center gap-2">
-      <div className="col-start-1 row-start-2 p-8 pl-10">
-        <h1 className="font-sans font-black text-5xl">
-          Construímos tus ambiciosos productos digitales.
-        </h1>
-        <p className="pt-5 text-justify text-lg">
-          Rango es una agencia de software. En Rango nosotros construímos el
-          futuro, resolvemos problemas y creamos las mejores experiencias en web
-          y móvil.
-        </p>
+    <>
+      <div className="grid grid-cols-2 grid-rows-3 justify-items-start gap-2">
+        <Navbar />
+        <div className="col-start-1 row-start-2 p-8 pl-12">
+          <h1 className="font-sans font-black text-5xl">
+            Construímos tus ambiciosos productos digitales.
+          </h1>
+          <p className="pt-6 text-lg">
+            Rango es una agencia de software. En Rango nosotros construímos el
+            futuro, resolvemos problemas y creamos las mejores experiencias en
+            web y móvil.
+          </p>
+        </div>
+        <div className="col-start-2 row-start-2 place-self-center">
+          <Image src="/rango_logo.png" width="300" height="300" />
+        </div>
+        <div className="col-start-1 row-start-3 p-4 pl-12">
+          <button className="bg-yellow-300 rounded-full py-4 px-7">
+            Contratános
+          </button>
+          <Link href="#">
+            <a className="underline px-5">Nuestro portafolio</a>
+          </Link>
+        </div>
       </div>
-      <div className="col-start-2 row-start-2">
-        <Image src="/rango_logo.png" width="300" height="300" />
-      </div>
-      <div className="col-start-1 row-start-3">
-        <button>Contratános</button>
-        <Link href="#">
-          <a>Nuestro portafolio</a>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
