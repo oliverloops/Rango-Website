1b93a9b80dbed036a2fb05770780c72093b9fd73
oliverloops <oliveralr@protonmail.com>
2021-07-02 13:24:02 -0500

    New layout updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { BsArrowDown } from "react-icons/bs";
+import { BiCodeAlt } from "react-icons/bi";
+import { GoServer } from "react-icons/go";
+import { MdWeb } from "react-icons/md";
+import { AiOutlineMobile } from "react-icons/ai";
 //UI components
 import Navbar from "../components/Navbar";
 
@@ -20,9 +24,9 @@ const Home = () => {
           </p>
         </div>
         <div className="col-start-2 row-start-1 place-self-center">
-          <Image src="/rango_logo.png" width="300" height="300" />
+          <Image src="/rango_logo.png" width="320" height="320" />
         </div>
-        <div className="col-start-1 row-start-2 p-4 pl-12">
+        <div className="col-start-1 row-start-2 pl-12">
           <Link href="#">
             <a>
               <button className="bg-yellow-300 rounded-full py-4 px-7">
@@ -53,6 +57,42 @@ const ServicesBlock = () => (
         mejorar alguna de tus plataformas o llevar tu visionaría idea a la
         realidad, nosotros lo hacemos por ti.
       </p>
+    </div>
+    <div className="col-start-2 row-start-1 place-self-center">
+      <div className="grid grid-auto-cols grid-auto-rows justify-items-center gap-6">
+        <div className="flex flex-col col-start-1 row-start-1 shadow-md rounded-2xl p-6">
+          <div className="bg-green-100 text-green-500 rounded-full w-20 h-20">
+            <BiCodeAlt size={32} />
+          </div>
+          <p className="text-sm text-center">
+            Desarrollo <br /> Frontend
+          </p>
+        </div>
+        <div className="flex flex-col col-start-1 row-start-2  shadow-md rounded-2xl p-6">
+          <div className="bg-purple-100 text-purple-500 rounded-full w-20 h-20">
+            <GoServer size={32} />
+          </div>
+          <p className="text-sm text-center">
+            Desarrollo <br /> Backend
+          </p>
+        </div>
+        <div className="flex flex-col col-start-2 row-start-1  shadow-md rounded-2xl p-6">
+          <div className="bg-red-100 text-red-600 rounded-full w-20 h-20">
+            <MdWeb size={32} />
+          </div>
+          <p className="text-sm text-center">
+            Diseño <br /> UI/UX
+          </p>
+        </div>
+        <div className="flex flex-col col-start-2 row-start-2  shadow-md rounded-2xl p-6">
+          <div className="bg-blue-100 text-blue-500 rounded-full w-20 h-20">
+            <AiOutlineMobile size={32} />
+          </div>
+          <p className="text-sm text-center">
+            Aplicaciones <br /> Móvil
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 );
