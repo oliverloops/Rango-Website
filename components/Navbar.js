7627159b6dfc112bed326a850df37327d59065e1
oliverloops <oliveralr@protonmail.com>
2021-07-02 12:38:06 -0500

    New layout updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
   return (
-    <nav className="col col-start-1 pl-10">
-      <p>hi</p>
+    <nav className="flex justify-between px-12 py-8 col-start-1">
+      <div>
+        <Link href="/">
+          <a>
+            <Image src="/rango_text.png" width="100" height="45" />
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="#">
+          <a className="px-4">Servicios</a>
+        </Link>
+        <Link href="#">
+          <a className="px-4">Portafolio</a>
+        </Link>
+        <Link href="#">
+          <a className="px-4">Acerca de Nosotros</a>
+        </Link>
+        <Link href="#">
+          <a>
+            <button className="bg-yellow-300 rounded-full py-4 px-7">
+              Contratános
+            </button>
+          </a>
+        </Link>
+      </div>
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import { BsArrowDown } from "react-icons/bs";
 //UI components
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
-      <div className="grid grid-cols-2 grid-rows-3 justify-items-start gap-2">
-        <Navbar />
-        <div className="col-start-1 row-start-2 p-8 pl-12">
+      <Navbar />
+      <div className="grid grid-cols-2 grid-auto-rows justify-items-start py-16 gap-2">
+        <div className="col-start-1 row-start-1 p-8 pl-12">
           <h1 className="font-sans font-black text-5xl">
             Construímos tus ambiciosos productos digitales.
           </h1>
@@ -18,20 +19,49 @@ const Home = () => {
             web y móvil.
           </p>
         </div>
-        <div className="col-start-2 row-start-2 place-self-center">
+        <div className="col-start-2 row-start-1 place-self-center">
           <Image src="/rango_logo.png" width="300" height="300" />
         </div>
-        <div className="col-start-1 row-start-3 p-4 pl-12">
-          <button className="bg-yellow-300 rounded-full py-4 px-7">
-            Contratános
-          </button>
+        <div className="col-start-1 row-start-2 p-4 pl-12">
+          <Link href="#">
+            <a>
+              <button className="bg-yellow-300 rounded-full py-4 px-7">
+                Contratános
+              </button>
+            </a>
+          </Link>
           <Link href="#">
             <a className="underline px-5">Nuestro portafolio</a>
           </Link>
         </div>
       </div>
+      <DownArrow />
+      <ServicesBlock />
+      <DownArrow />
     </>
   );
 };
 
+const ServicesBlock = () => (
+  <div className="grid grid-cols-2 grid-auto-rows justify-items-start py-12 gap-2">
+    <div className="col-start-1 row-start-1 p-8 pl-12">
+      <h1 className="font-sans font-black text-5xl">
+        Hacemos tus ideas realidad con alguno de nuestros servicios.
+      </h1>
+      <p className="pt-6 text-lg">
+        Ya sea que necesites un sitio web que se adapte a tus necesidades,
+        mejorar alguna de tus plataformas o llevar tu visionaría idea a la
+        realidad, nosotros lo hacemos por ti.
+      </p>
+    </div>
+  </div>
+);
+
+const DownArrow = () => (
+  <div className="flex justify-center px-8 py-14">
+    <svg className="animate-bounce w-6 h-6">
+      <BsArrowDown size={26} />
+    </svg>
+  </div>
+);
 export default Home;
