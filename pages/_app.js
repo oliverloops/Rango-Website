d270de1abcaf2443df86a7c7e1052786a3cfedfe
oliverloops <oliveralr@protonmail.com>
2021-07-19 10:26:35 -0500

    Open Graph metatags added

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
   return (
-    <FormspreeProvider project="1723073748489207795">
+    <FormspreeProvider project="1725937456705961015">
       <Component {...pageProps} />
     </FormspreeProvider>
   );
